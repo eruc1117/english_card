c357f5d0dccfce7ebcd229fdf778166abc51e149
eruc <eruc101010@gmail.com>
2022-04-02 14:22:19 +0800

    feat: add basic web structure

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const sassMiddleware = require('node-sass-middleware')
+const path = require('path')
+const PORT = 3000
+
+const handlebars = exphbs.create({
+})
+app.engine('.handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+  })
+)
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+});
