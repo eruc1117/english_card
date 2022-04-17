91cf980f7074f890795125aed352fea144ab6875
eruc <eruc101010@gmail.com>
2022-04-17 11:16:40 +0800

    feat: add authentication and logout functions

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 const enCrud = require('./models/enCardController')
 const login = require('./models/login')
 const register = require('./models/register')
+const logout = require('./models/logout')
 const testSetting = require('./models/testSetting')
 const test = require('./models/test')
 const home = require('./models/home')
 const enWordList = require('./models/wordList')
 
-router.use('/enCrud', enCrud)
 router.use('/login', login)
+router.use('/logout', logout)
 router.use('/register', register)
-router.use('/testSetting', testSetting)
-router.use('/test', test)
-router.use('/enWordList', enWordList)
+router.use('/enWordList', authenticator, enWordList)
+router.use('/enCrud', authenticator, enCrud)
+router.use('/testSetting', authenticator, testSetting)
+router.use('/test', authenticator, test)
 router.use('/', home)
 
 
