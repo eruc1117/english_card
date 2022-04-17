b75033deac3cb99c09e60fb372a9a81b43a1a39e
eruc <eruc101010@gmail.com>
2022-04-17 10:10:18 +0800

    feat: add register function

--- routes/models/login.js
+++ routes/models/login.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/enCrud', () => {
-  res.render('index')
+router.get('/', () => {
+  res.render('login')
 })
 
 module.exports = router
--- routes/models/register.js
+++ routes/models/register.js
@@ -1,8 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const PORT = 3000
+const bcrypt = require('bcryptjs')
+const User = require('../../models/UserModel')
 
-router.get('/enCrud', () => {
-  res.render('index')
+const customize = require('../../function/constructor')
+const registerCss = new customize.PageCss('register', PORT)
+
+router.get('/', (req, res) => {
+  res.render('register', { cssStyle: registerCss.css})
+})
+
+router.post('/', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  if (password !== confirmPassword) {
+    return res.redirect('/user/rigster')
+  }
+  async function rigster(name, email, password) {
+    const userInfo = await User.findOne({ email })
+    const lastId = await User.find()
+    let newUser = { name, email }
+    if (userInfo) {
+      const errorMsg = `這個 Email 已經註冊過了。`
+      return res.render('rigster', { userInfo: newUser, error_msg: errorMsg, cssStyle: style.css })
+    }
+    if (!userInfo && !lastId[lastId.length - 1]) {//[lastId.length - 1]，在node v16後可以用 .at(-1)替代 
+      newUser.id = 1
+    } else {
+      newUser.id = Number(lastId[lastId.length - 1].id) + 1
+    }
+    const salt = await bcrypt.genSalt(10)
+    newUser.password = await bcrypt.hash(password, salt)
+    newUser.name = name
+    await User.create(newUser)
+    return res.redirect('/')
+  }
+  rigster(name, email, password)
 })
 
 module.exports = router
