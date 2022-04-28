5ced8f0ada3cee47cd7339a2bf51e26179f1a5d8
eruc <eruc101010@gmail.com>
2022-04-28 09:22:57 +0800

    fix: change css link name

--- function/constructor.js
+++ function/constructor.js
@@ -1,9 +1,5 @@
 function PageCss(name, env) {
-  if (env !== 3000) {
-    this.css = `https://eruc-english-card.herokuapp.com/stylesheets/${name}.css`
-  } else {
-    this.css = `https://localhost:3000/stylesheets/${name}.css`
-  }
+  this.css = `/stylesheets/${name}.css`
 }
 
 module.exports = {
--- routes/models/home.js
+++ routes/models/home.js
@@ -4,7 +4,7 @@ const PORT = 3000
 
 const customize = require('../../function/constructor')
 const home = new customize.PageCss('home', PORT)
-
+console.log(home.css)
 router.get('/', (req, res) => {
   res.render('home', { cssStyle: home.css })
 })
